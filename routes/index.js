48ed2c5c009427b37fac593c9db8391d3d716693
Kelvin Omereshone <kelvinomereshone@gmail.com>
2019-10-28 23:40:31 +0100

    Chore: cleaned up boiler plate to remove irrelevant codes, folders and files

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  res.json({
+    message:
+      "Welcome to Naija Presidents Web Service. Resources are available at /presidents"
+  });
 });
 
 module.exports = router;
